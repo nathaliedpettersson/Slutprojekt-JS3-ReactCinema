707d2abe751253a313fa05919eab9ba7a33b10ee
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2022-11-22 11:58:41 +0100

    Fixing new solution for /-page
    Trying new code so that site won't crash when movies in localStorage is empty. Not yet done and tested

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,32 @@ const App = () => {
     navigate("/profile")
   }
 
+  const displayMovies = localStorage.getItem("movies") ? JSON.parse(localStorage.getItem("movies")) : null;
+
   return (
     <div className="main-page">
       <h1 className="main-page-header">CINEMA APP REACT</h1>
+      <div className="btn-wrapper">
       <Button handleClick={goToProfile} btnText={"Add movie"} />
-      {/* <DisplayMovie /> */}
+      </div>
+       {/* <DisplayMovie />  */}
+
+    <div className="display-movie-app-container">
+    <ul className="display-movies-app">
+      {displayMovies.map((movie, index) => {
+        return (
+          <li key={index}>
+            <h3>{movie.title}</h3>
+          <img className="movies-img-app" src={movie.img} alt="Movie img"></img>
+          <p>{movie.summary}</p>
+          </li>
+        )
+      })}
+    </ul>
+    </div>
     </div>
   )
 }
 
 export default App;
+
