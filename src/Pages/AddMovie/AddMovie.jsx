806eb02a55cede1f7f0efe692a3e389f5f4a7c51
nathaliedpettersson <nathalie.pettersson90@gmail.com>
2023-04-24 13:44:06 +0200

    refactor: delete comments and format code

--- src/Pages/AddMovie/AddMovie.jsx
+++ src/Pages/AddMovie/AddMovie.jsx
@@ -1,55 +1,74 @@
-import React from 'react';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddMovie = () => {
+  const [movies, setMovies] = useState(
+    localStorage.getItem("movies")
+      ? JSON.parse(localStorage.getItem("movies"))
+      : []
+  );
 
-    // Using useState to get movies from localStorage or create an empty array if it doesn't exist
-    const [movies, setMovies] = useState(localStorage.getItem("movies")
-        ? JSON.parse(localStorage.getItem("movies")) : []);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const defaultImg =
+    "https://img.freepik.com/premium-vector/cinema-movie-background-popcorn-filmstrip-clapboard-tickets-movie-time-background_41737-248.jpg";
 
-    const defaultImg = 'https://img.freepik.com/premium-vector/cinema-movie-background-popcorn-filmstrip-clapboard-tickets-movie-time-background_41737-248.jpg';
+  const movieObject = {
+    title: "",
+    img: "" || defaultImg,
+    summary: "",
+    longDesc: "",
+    user: "",
+  };
 
-    // Movieobject to be sent to localStorage so that we can get access to it in display movies
-    const movieObject = {
-        title: '',
-        img: '' || defaultImg,
-        summary: '',
-        longDesc: '',
-        user: ''
-    }
+  const submitNewMovie = (e) => {
+    e.preventDefault();
+    const loggedInUser = localStorage.getItem("Authorized");
+    movieObject.user = loggedInUser;
 
-    // We set movieObject to movie array and save it in state and then also set the new item to localstorage
-    const submitNewMovie = (e) => {
-        e.preventDefault();
-        const loggedInUser = localStorage.getItem('Authorized');
-        movieObject.user = loggedInUser;
+    setMovies([...movies, movieObject]);
+    localStorage.setItem("movies", JSON.stringify([...movies, movieObject]));
 
-        setMovies([...movies, movieObject])
-        localStorage.setItem("movies", JSON.stringify([...movies, movieObject]));
+    navigate("/movies");
+  };
 
-        navigate("/movies");
-    }
-
-
-    return (
-        <div className="add-movie-container">
-            <h3 className="add-movie-header">You're in! Add movies and get inspo from other users, it'll be fun.</h3>
-            <form onSubmit={(e) => submitNewMovie(e)}>
-                <input className="add-movie-input" type="text" placeholder="Title" onChange={(e) => movieObject.title = e.target.value} />
-                <input className="add-movie-input" type="url" placeholder="Url (optional)" onChange={(e) => movieObject.img = e.target.value} />
-                <input className="add-movie-input" type="text" placeholder="Short summary" onChange={(e) => movieObject.summary = e.target.value} />
-                <input className="add-movie-input" type="text" placeholder="Description" onChange={(e) => movieObject.longDesc = e.target.value} />
-                <br></br>
-                <br></br>
-                <input className="add-movie-btn" type="submit" value="Add movie" />
-            </form>
-        </div>
-
-
-    )
-}
+  return (
+    <div className="add-movie-container">
+      <h3 className="add-movie-header">
+        You're in! Add movies and get inspo from other users, it'll be fun.
+      </h3>
+      <form onSubmit={(e) => submitNewMovie(e)}>
+        <input
+          className="add-movie-input"
+          type="text"
+          placeholder="Title"
+          onChange={(e) => (movieObject.title = e.target.value)}
+        />
+        <input
+          className="add-movie-input"
+          type="url"
+          placeholder="Url (optional)"
+          onChange={(e) => (movieObject.img = e.target.value)}
+        />
+        <input
+          className="add-movie-input"
+          type="text"
+          placeholder="Short summary"
+          onChange={(e) => (movieObject.summary = e.target.value)}
+        />
+        <input
+          className="add-movie-input"
+          type="text"
+          placeholder="Description"
+          onChange={(e) => (movieObject.longDesc = e.target.value)}
+        />
+        <br></br>
+        <br></br>
+        <input className="add-movie-btn" type="submit" value="Add movie" />
+      </form>
+    </div>
+  );
+};
 
 export default AddMovie;
